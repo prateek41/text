5a7b13efea80f2faca4939a59dfa02f0c5e6b3d5
Prateek Bhagtani <71914478+prateek41@users.noreply.github.com>
2022-01-23 02:06:10 +0530

    Update urls.py

--- textutils/urls.py
+++ textutils/urls.py
@@ -14,13 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from . import views
+
+from django.conf import settings
+from django.views.static import serve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('analyze', views.analyze, name='analyze'),
-    path('about', views.about, name='about')
+    path('about', views.about, name='about'),
+
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 
 ]
